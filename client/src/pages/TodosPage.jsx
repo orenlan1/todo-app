3eb1978cbda1733d636b85ea5adb25193b0c1452
oren lan <lanoren1@gmail.com>
2025-03-19 10:57:48 +0200

    work on complete todo

--- client/src/pages/TodosPage.jsx
+++ client/src/pages/TodosPage.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import Todo from '../components/Todo';
 import Modal from '../components/Modal';
 
-function TodosPage({userId, setUserId}) {
+function TodosPage({userId}) {
     
     const [todos, setTodos] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -68,7 +68,7 @@ function TodosPage({userId, setUserId}) {
                 <div className='text-center text-4xl pt-10 rounded-xl'>
                     <h1>TODO LIST</h1>
                 </div>
-                <div>
+                <div className='ml-5 mt-5'>
                     <button onClick={() => openModal()} className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 
                     py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>Add New</button>
                     <button className='text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4
